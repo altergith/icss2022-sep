1dc0a30f9f08d1604ff8c9c055603c94755ab157
Alper Bekar <a.bekar@student.han.nl>
2024-10-22 15:33:58 +0200

    Feat: Generator generates multiple stylerules with multiple declaration

--- startcode/src/main/java/nl/han/ica/icss/generator/Generator.java
+++ startcode/src/main/java/nl/han/ica/icss/generator/Generator.java
@@ -2,8 +2,14 @@ package nl.han.ica.icss.generator;
 
 
 import nl.han.ica.icss.ast.*;
+import nl.han.ica.icss.ast.literals.ColorLiteral;
+import nl.han.ica.icss.ast.literals.PercentageLiteral;
+import nl.han.ica.icss.ast.literals.PixelLiteral;
+import org.checkerframework.checker.units.qual.C;
 
+import java.awt.*;
 import java.util.ArrayList;
+import java.util.Objects;
 
 public class Generator {
 
@@ -14,18 +20,49 @@ public class Generator {
 	}
 
     private String generateStylesheet(Stylesheet node) {
-        return generateStyleRule((Stylerule) node.getChildren().get(0));
+        String result = "";
+        for (ASTNode child :node.getChildren()
+             ) {
+            result += generateStyleRule((Stylerule) child);
+            result += "\n";
+        }
+        return result;
     }
 
     private String generateStyleRule(Stylerule stylerule) {
-        String result = stylerule.selectors.get(0) + " {\n ";
-        result += "\t" + generateDecleration((Declaration)stylerule.body.get(0));
-        result += "\n}";
+        String result = "";
+//        for (Selector sa:stylerule.selectors
+//             ) {
+//
+//        }
+        result += stylerule.selectors.get(0) + " {\n ";
+        for (ASTNode child: stylerule.body
+             ) {
+            result += "\t";
+            result += generateDecleration((Declaration) child);
+            result += "\n";
+        }
+        result += "}";
         return result;
     }
 
     private String generateDecleration(Declaration node) {
-        return "decleration";
+        String result = "";
+        if (node.expression instanceof ColorLiteral){
+            ColorLiteral l =  (ColorLiteral) node.expression;
+            result += node.property.name + ": " + l.value;
+        }
+
+        if (node.expression instanceof PixelLiteral){
+            PixelLiteral l =  (PixelLiteral) node.expression;
+            result += node.property.name + ": " + l.value;
+        }
+
+        if (node.expression instanceof PercentageLiteral){
+            PercentageLiteral l =  (PercentageLiteral) node.expression;
+            result += node.property.name + ": " + l.value;
+        }
+        return result;
     }
 
 
